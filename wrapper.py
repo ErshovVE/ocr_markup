8176eb2e39b9e6878bbf4b7510369ef4616ab826
Vladislav Ershov <75930878+ErshovVE@users.noreply.github.com>
2025-08-28 17:21:44 +0200

    feat: add pyinstaller cmd

---
+++ wrapper.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+from streamlit.web import cli as stcli
+
+
+def resource_path(relative_path):
+    """ Получить путь к ресурсу при сборке в .exe """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+   
+
+if __name__ == "__main__":
+    app_path = resource_path("app.py")
+    sys.argv = [
+        "streamlit", 
+        "run", 
+        app_path,
+        "--global.developmentMode=false",
+        "--server.headless=true"
+    ]
+    
+    # Запуск Streamlit
+    sys.exit(stcli.main())
